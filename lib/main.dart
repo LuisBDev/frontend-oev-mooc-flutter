b1ae3f2ed0eebdeab8beaa0e8a6c33cf5358d524
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-01-27 16:58:29 -0500

    logout for development

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:oev_mobile_app/config/constants/environment.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oev_mobile_app/config/router/app_router.dart';
+import 'package:oev_mobile_app/presentation/screens/login/providers/auth_provider.dart';
 
 void main() async {
   await Environment.initEnvironment();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -13,8 +15,8 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // ref.watch(authProvider.notifier).logout();
     final appRouter = ref.watch(goRouterProvider);
-
     return MaterialApp.router(
       title: 'OEV App',
       routerConfig: appRouter,
